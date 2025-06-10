6832cafdf57e5384596229b9d4c11fb5d9cc4b98
Domingos Braganha <domingosbreganha9@gmail.com>
2025-06-10 06:55:50 -0300

    feat: criar ficheiros de indicadores

---
+++ app/Models/Indicator/Indicator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Indicator;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Indicator extends Model
+{
+    use HasFactory;
+    protected $table = 'indicator';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name', 'description', 'created_at', 'updated_a2t'];
+}

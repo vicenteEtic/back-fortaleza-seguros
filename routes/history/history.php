95dc30cf4aedce3f491e4034509c490ab71ba8fa
root <vicentemanueleduardo@gmail.com>
2025-06-11 15:17:41 +0100

    seed para indicadores

--- routes/history/history.php
+++ routes/history/history.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\Log\LogController;
 Route::get('', [LogController::class, 'index'])
     ->name('history.index');
 
-Route::post('', [LogController::class, 'store'])
-    ->name('history.store');
+    Route::post('', [LogController::class, 'store'])->name('history.store');
+
 
 Route::get('{history}', [LogController::class, 'show'])
     ->name('history.show');

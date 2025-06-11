452356ef68d1d41af4544684a70245f3a9c949c6
root <vicentemanueleduardo@gmail.com>
2025-06-11 11:43:03 +0100

    validacao dos dados

---
+++ database/migrations/2025_06_10_144035_create_indicator_type_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('indicator_type', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('description');
+            $table->string('comment');
+            $table->string('risk');
+            $table->foreignId('indicator_id')
+                ->constrained('indicator')
+                ->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('indicator_type');
+    }
+};

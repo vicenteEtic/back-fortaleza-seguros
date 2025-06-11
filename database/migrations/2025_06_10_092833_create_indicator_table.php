6ae019197a6c98cdd071c49d64fdb294a7206384
Domingos Braganha <domingosbreganha9@gmail.com>
2025-06-11 07:32:35 -0300

    fix: usar softdelete

--- database/migrations/2025_06_10_092833_create_indicator_table.php
+++ database/migrations/2025_06_10_092833_create_indicator_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('description')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }

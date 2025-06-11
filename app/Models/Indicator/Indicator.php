0a7976c6fdbf43159ac8b0e62fbca0a00a4f91bf
Domingos Braganha <domingosbreganha9@gmail.com>
2025-06-11 07:19:38 -0300

    fix: usar namepsaces

--- app/Models/Indicator/Indicator.php
+++ app/Models/Indicator/Indicator.php
@@ -4,6 +4,7 @@ namespace App\Models\Indicator;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Indicator extends Model
 {

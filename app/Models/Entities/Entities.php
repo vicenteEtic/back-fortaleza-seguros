813d99820fccce1a2bc28818fcb329416fdf20aa
Domingos Braganha <domingosbreganha9@gmail.com>
2025-06-11 07:19:38 -0300

    fix: usar namepsaces

--- app/Models/Entities/Entities.php
+++ app/Models/Entities/Entities.php
@@ -4,10 +4,11 @@ namespace App\Models\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Entities extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = 'entities';
     protected $primaryKey = 'id';
     protected $fillable = ['social_denomination', 'policy_number', 'customer_number', 'entity_type', 'color', 'risk_level', 'diligence', 'last_evaluation', 'deleted_at', 'created_at', 'updated_at'];

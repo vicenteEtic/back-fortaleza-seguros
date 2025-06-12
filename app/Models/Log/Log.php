cc3261966bb1d2c8d991c81cb5760b46b0be5120
root <vicentemanueleduardo@gmail.com>
2025-06-12 14:38:51 +0100

    feat: log de atividades global

--- app/Models/Log/Log.php
+++ app/Models/Log/Log.php
@@ -2,27 +2,13 @@
 
 namespace App\Models\Log;
 
-use App\Models\User as ModelsUser;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Log extends Model
 {
-    use HasFactory, SoftDeletes;
-
-    protected $table = 'user_logs';
-
-    protected $guarded = ['id'];
-     /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = ['deleted_at'];
-
-    public function user(){
-        return $this->belongsTo(ModelsUser::class, 'USER_ID', 'id');
-    }
-
+    use HasFactory;
+    protected $table = 'log';
+    protected $primaryKey = 'id';
+    protected $fillable = ['level', 'REMOTE_ADDR', 'PATH_INFO', 'USER_NAME', 'USER_ID', 'HTTP_USER_AGENT', 'message', 'id_document'];
 }

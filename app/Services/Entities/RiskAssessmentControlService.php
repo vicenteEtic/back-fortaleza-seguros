85b73ec214bfe8f41b0df6b5c8bb16154c20a112
Domingos Braganha <domingosbreganha9@gmail.com>
2025-07-03 05:06:13 -0300

    fix: retornar relacionamento

--- app/Services/Entities/RiskAssessmentControlService.php
+++ app/Services/Entities/RiskAssessmentControlService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Services\Entities;
 
 use App\Repositories\Entities\RiskAssessmentControlRepository;
@@ -9,5 +10,14 @@ class RiskAssessmentControlService extends AbstractService
     public function __construct(RiskAssessmentControlRepository $repository)
     {
         parent::__construct($repository);
+    }
+
+    public function index(?int $paginate, ?array $filterParams, ?array $orderByParams, $relationships = [])
+    {
+        $relationships = [
+            'user'
+        ];
+        $orderByParams = $orderByParams ?? ['created_at' => 'desc'];
+        return $this->repository->index($paginate, $filterParams, $orderByParams, $relationships);
     }
 }

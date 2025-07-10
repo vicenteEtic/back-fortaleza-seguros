3b1cf5607966f1015f2cd7f4122bd6e89785a594
Domingos Braganha <domingosbreganha9@gmail.com>
2025-07-10 12:26:48 -0300

    feat: resetar senha

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -24,7 +24,7 @@ class NewPasswordController extends Controller
         $request->validate([
             'token' => ['required'],
             'email' => ['required', 'email'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', Rules\Password::defaults()],
         ]);
 
         // Here we will attempt to reset the user's password. If it is successful we

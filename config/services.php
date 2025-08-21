eaef9b6030f9cc42607e787e802e079649d1bb95
root <vicentemanueleduardo@gmail.com>
2025-08-21 17:48:18 +0100

    fix:erro ao gerar alertas

--- config/services.php
+++ config/services.php
@@ -6,12 +6,6 @@ return [
     |--------------------------------------------------------------------------
     | Third Party Services
     |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
     */
 
     'postmark' => [
@@ -33,6 +27,10 @@ return [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
+    ],
+
+    'pep' => [
+        'url' => env('URL_PEP_API', 'http://172.17.100.11:1090/api'),
     ],
 
 ];

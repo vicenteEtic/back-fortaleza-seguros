d6abd76630e74361bf459267bdb107f5caa9adac
root <vicentemanueleduardo@gmail.com>
2025-08-26 10:27:32 +0100

    feat:add        ProfessionNossaSeguros

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,23 +13,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         User::factory(2)->create();
+        //   User::factory(2)->create();
 
-        $this->call(DiligenceSeeder::class);
-        $this->call(IndicatorSeeder::class);
-        $this->call(IdentificationCapacitySeeder::class);
-        $this->call(ResidenceSeed::class);
-        $this->call(EstablishmentSeed::class);
-        $this->call(LegalFormSeed::class);
-        $this->call(CAESeed::class);
-        $this->call(PEPSeed::class);
-        $this->call(InsuranceTypeSeed::class);
-        $this->call(CountriesSeed::class);
-        $this->call(CountriesSeed::class);
-        $this->call(TypeActivitySeed::class);
-        $this->call(ProfissionPSeed::class);
-        $this->call(ProductRiskSeeder::class);
-        $this->call(ChannelSeed::class);
-        $this->call(PermissionSeed::class);
+
+        // $this->call(DiligenceSeeder::class);
+        // $this->call(IndicatorSeeder::class);
+        // $this->call(IdentificationCapacitySeeder::class);
+        // $this->call(ResidenceSeed::class);
+        // $this->call(EstablishmentSeed::class);
+        //  $this->call(LegalFormSeed::class);
+        //  $this->call(CAESeed::class);
+        //  $this->call(PEPSeed::class);
+        //  $this->call(InsuranceTypeSeed::class);
+        //  $this->call(CountriesSeed::class);
+        // $this->call(CountriesSeed::class);
+        // $this->call(TypeActivitySeed::class);
+        //    $this->call(ProfissionPSeed::class);
+        $this->call(ProfessionNossaSeguros::class);
+        //  $this->call(ProductRiskSeeder::class);
+        //  $this->call(ChannelSeed::class);
+        //  $this->call(PermissionSeed::class);
     }
 }

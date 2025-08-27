661ad51e22dbb83a4c8d0a71df6dd51f8583f7b5
root <vicentemanueleduardo@gmail.com>
2025-08-27 16:11:23 +0100

    feat:modificacao dos alertas organizacao dos alertas

--- app/Services/Alert/AlertUser/AlertUserService.php
+++ app/Services/Alert/AlertUser/AlertUserService.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Services\Alert\AlertUser;
 
+use App\Models\User;
 use App\Repositories\Alert\AlertUser\AlertUserRepository;
 use App\Services\AbstractService;
 
@@ -9,5 +10,41 @@ class AlertUserService extends AbstractService
     public function __construct(AlertUserRepository $repository)
     {
         parent::__construct($repository);
+    }
+
+    /**
+     * Resumo de alertas de UM usuário
+     */
+    public function getUserAlertSummary($userId)
+    {
+        $active   = $this->repository->countActiveAlertsByUser($userId);
+        $inactive = $this->repository->countInactiveAlertsByUser($userId);
+
+        return [
+            'user_id'        => $userId,
+            'active_alerts'  => $active,
+            'inactive_alerts'=> $inactive,
+        ];
+    }
+
+    /**
+     * Resumo de alertas de TODOS os usuários relacionados a alertas
+     */
+    public function getAllUsersAlertSummary()
+    {
+        // busca todos os usuários que têm alertas associados
+        $userIds = $this->repository->getUsersWithAlerts();
+
+        return collect($userIds)->map(function ($userId) {
+            $user = User::find($userId);
+
+            return [
+                'id'             => $user->id,
+                'name'           => $user->first_name.' '.$user->last_name,
+                'email'          => $user->email,
+                'active_alerts'  => $this->repository->countActiveAlertsByUser($userId),
+                'inactive_alerts'=> $this->repository->countInactiveAlertsByUser($userId),
+            ];
+        })->values();
     }
 }

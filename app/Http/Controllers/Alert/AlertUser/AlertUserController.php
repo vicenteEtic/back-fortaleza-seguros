ef19eca331d50d5402fc0f3519a6c04e06daabe1
root <vicentemanueleduardo@gmail.com>
2025-08-27 16:11:23 +0100

    feat:modificacao dos alertas organizacao dos alertas

--- app/Http/Controllers/Alert/AlertUser/AlertUserController.php
+++ app/Http/Controllers/Alert/AlertUser/AlertUserController.php
@@ -1,51 +1,82 @@
 <?php
-    
-    namespace App\Http\Controllers\Alert\AlertUser;
-    
-    use App\Http\Controllers\AbstractController;
-    use App\Services\Alert\AlertUser\AlertUserService;
-    use App\Http\Requests\Alert\AlertUser\AlertUserRequest;
-    use Exception;
-    use Illuminate\Database\Eloquent\ModelNotFoundException;
-    use Illuminate\Http\Response;
-    
-    class AlertUserController extends AbstractController
+
+namespace App\Http\Controllers\Alert\AlertUser;
+
+use App\Http\Controllers\AbstractController;
+use App\Services\Alert\AlertUser\AlertUserService;
+use App\Http\Requests\Alert\AlertUser\AlertUserRequest;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use function PHPUnit\Framework\returnSelf;
+
+class AlertUserController extends AbstractController
+{
+    public function __construct(AlertUserService $service)
     {
-        public function __construct(AlertUserService $service)
-        {
-            $this->service = $service;
-        }
-    
-        /**
-         * Store a newly created resource in storage.
-         */
-        public function store(AlertUserRequest $request)
-        {
-            try {
+        $this->service = $service;
+    }
+
+
+    public function index(Request $request)
+    {
+
+        try {
+            if ($this->logRequest) {
                 $this->logRequest();
-                $alertUser = $this->service->store($request->validated());
-                return response()->json($alertUser, Response::HTTP_CREATED);
-            } catch (Exception $e) {
-                $this->logRequest($e);
-                return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+                $this->logToDatabase(
+                    type: $this->logType,
+                    level: 'info',
+                    customMessage: "O usuario " . Auth::user()->first_name . " Visualizou todos os registros no módulo {$this->nameEntity}",
+                );
             }
-        }
-    
-        /**
-         * Update the specified resource in storage.
-         */
-        public function update(AlertUserRequest $request, $id)
-        {
-            try {
-                $this->logRequest();
-                $alertUser = $this->service->update($request->validated(), $id);
-                return response()->json($alertUser, Response::HTTP_OK);
-            } catch (ModelNotFoundException $e) {
+
+            $filters = $request['filters'] ?? $request['filtersV2'];
+            $service = $this->service->index($request['paginate'], $filters, $request['orderBy'], $request['relationships']);
+
+            return $users = $this->service->getAllUsersAlertSummary();
+        } catch (Exception $e) {
+            if ($this->logRequest) {
                 $this->logRequest($e);
-                return response()->json(['error' => 'Resource not found.'], Response::HTTP_NOT_FOUND);
-            } catch (Exception $e) {
-                $this->logRequest($e);
-                return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
             }
+            return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+
+    public function store(AlertUserRequest $request)
+    {
+        try {
+            $this->logRequest();
+            $alertUser = $this->service->store($request->validated());
+            return response()->json($alertUser, Response::HTTP_CREATED);
+        } catch (Exception $e) {
+            $this->logRequest($e);
+            return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(AlertUserRequest $request, $id)
+    {
+        try {
+            $this->logRequest();
+            $alertUser = $this->service->update($request->validated(), $id);
+            return response()->json($alertUser, Response::HTTP_OK);
+        } catch (ModelNotFoundException $e) {
+            $this->logRequest($e);
+            return response()->json(['error' => 'Resource not found.'], Response::HTTP_NOT_FOUND);
+        } catch (Exception $e) {
+            $this->logRequest($e);
+            return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- app/Repositories/Alert/AlertUser/AlertUserRepository.php
+++ app/Repositories/Alert/AlertUser/AlertUserRepository.php
@@ -10,4 +10,30 @@ class AlertUserRepository extends AbstractRepository
     {
         parent::__construct($model);
     }
+
+    public function countActiveAlertsByUser($userId)
+    {
+        return $this->model
+            ->where('user_id', $userId)
+            ->whereHas('alert', function ($q) {
+                $q->where('is_active', 1);
+            })
+            ->count();
+    }
+
+    public function countInactiveAlertsByUser($userId)
+    {
+        return $this->model
+            ->where('user_id', $userId)
+            ->whereHas('alert', function ($q) {
+                $q->where('is_active', 0);
+            })
+            ->count();
+    }
+    public function getUsersWithAlerts()
+{
+    return $this->model
+        ->distinct()
+        ->pluck('user_id');
+}
 }

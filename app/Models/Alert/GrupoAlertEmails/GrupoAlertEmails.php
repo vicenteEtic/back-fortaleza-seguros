c90430e5858a7cfc5e8fa3fd17adaca07f76d30d
root <vicentemanueleduardo@gmail.com>
2025-09-03 14:59:26 +0100

    feat:add in alert in GrupoType

--- app/Models/Alert/GrupoAlertEmails/GrupoAlertEmails.php
+++ app/Models/Alert/GrupoAlertEmails/GrupoAlertEmails.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Alert\GrupoAlertEmails;
 
+use App\Models\Alert\GrupoType\GrupoType;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -12,5 +13,9 @@ class GrupoAlertEmails extends Model
     protected $table = 'grupo_alert_emails';
     protected $primaryKey = 'id';
     protected $fillable = ['name','description'];
+    public function GrupoType()
+    {
+        return $this->hasMany(GrupoType::class, 'grup_alert_id');
+    }
 }
 

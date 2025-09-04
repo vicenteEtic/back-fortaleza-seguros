5c493e81260910cdf6b7eff63eba6822780ff6a0
root <vicentemanueleduardo@gmail.com>
2025-09-04 17:17:43 +0100

    feat: add list of listTypGrup

--- app/Services/Alert/GrupoType/GrupoTypeService.php
+++ app/Services/Alert/GrupoType/GrupoTypeService.php
@@ -10,4 +10,14 @@ class GrupoTypeService extends AbstractService
     {
         parent::__construct($repository);
     }
+    public function listTypGrup(){
+        return [
+            ['name' => "PEP", 'info_pt' => "Pessoa Politicamente Exposta"],
+            ['name' => "Sanctions List", 'info_pt' => "Lista de Sanções"],
+            ['name' => "Transaction Monitoring", 'info_pt' => "Monitoramento de Transações"],
+            ['name' => "Customer Profile and Behavior", 'info_pt' => "Perfil e Comportamento do Cliente"],
+            ['name' => "Linked to Award and Payment", 'info_pt' => "Ligados ao Prémio e Pagamento"],
+        ];
+        
+    }
 }

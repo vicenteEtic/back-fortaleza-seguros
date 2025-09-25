bc473a76c99da7856730386ac898f3d8b17595ba
root <vicentemanueleduardo@gmail.com>
2025-09-25 19:11:57 +0100

    feat:update alert

--- app/Http/Requests/Alert/AlertUpdateStatusRequest.php
+++ app/Http/Requests/Alert/AlertUpdateStatusRequest.php
@@ -2,37 +2,35 @@
 
 namespace App\Http\Requests\Alert;
 
-use App\Http\Requests\BaseFormRequest;
-use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
 
-class AlertUpdateStatusRequest extends BaseFormRequest
+class AlertUpdateStatusRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return true;
+        return false;
     }
 
     /**
      * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            'is_active' => ['required', 'boolean'], // melhor validar como booleano
+            'is_active' => ['required'], // melhor validar como booleano
         ];
     }
 
-    /**
-     * Custom error messages.
-     */
     public function messages(): array
     {
         return [
             'is_active.required' => 'O campo is_active é obrigatório.',
-            'is_active.boolean'  => 'O campo is_active deve ser verdadeiro ou falso.',
+
         ];
     }
 }

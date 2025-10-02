091bb18136ed1d5ff82c795e7264fbe1cee84f7d
vicenteeduardo <vicentemanueleduardo@gmail.com>
2025-10-02 11:53:14 +0100

    2

--- app/Http/Requests/User/ChangePasswordRequest.php
+++ app/Http/Requests/User/ChangePasswordRequest.php
@@ -3,30 +3,18 @@
 namespace App\Http\Requests\User;
 
 use App\Http\Requests\BaseFormRequest;
-use Illuminate\Validation\Validator;
+
 class ChangePasswordRequest extends BaseFormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-  public function rules()
-{
-    return [
-        'new_password' => 'required|password_requirements|confirmed',
-    ];
-}
-
-
-
-
+    public function rules(): array
+    {
+        return [
+            'new_password' => 'required|string|min:8|confirmed',
+        ];
+    }
 }
